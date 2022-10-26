ccb39d7ec65e7005bbdcefd81ef18be0c034788f
Latrevion <xiaopikaqiu@outlook.com>
2022-10-26 16:42:08 +0800

    搭建项目初始框架

---
+++ src/components/Config.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    配置页面
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        你好
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+@import "../views/_helper.scss";
+
+</style>
