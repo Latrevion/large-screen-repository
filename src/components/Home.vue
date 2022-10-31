c91e2e981635214889366cb2eb89f323836ab717
Latrevion <xiaopikaqiu@outlook.com>
2022-10-31 12:11:37 +0800

    make grids

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        <header class="x" :style="{backgroundImage:`url(${imgUrl})`}"></header>
+    <div class="home">
+        <header class="header" :style="{ backgroundImage: `url(${imgUrl})` }"></header>
+        <main class="main">
+            <section class="section1"></section>
+            <section class="section2"></section>
+            <section class="section3"></section>
+            <section class="section4"></section>
+            <section class="section5"></section>
+        </main>
     </div>
 </template>
 <script>
@@ -13,11 +20,51 @@ export default {
     }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../views/_helper.scss";
 
-.x {
-    background-size: cover;
-    height: px(99);
-}
+ .home {
+     flex: 1;
+     height: 100%;
+     display: flex;
+     flex-direction: column;
+
+     >.header {
+         background-size: cover;
+         height: px(99);
+     }
+
+     >.main {
+         flex: 1;
+         display: grid;
+         grid-template:
+             "box1 box2 box4 box5" 755fr
+             "box3 box3 box4 box5" 363fr / 366fr 361fr 811fr 747fr;
+
+         >.section1 {
+             grid-area: box1;
+             background: pink;
+         }
+
+         >.section2 {
+             grid-area: box2;
+             background: lightgray;
+         }
+
+         >.section3 {
+             grid-area: box3;
+             background: lightblue;
+         }
+
+         >.section4 {
+             grid-area: box4;
+             background: lightcyan;
+         }
+
+         >.section5 {
+             grid-area: box5;
+             background: lightyellow;
+         }
+     }
+ }
 </style>
