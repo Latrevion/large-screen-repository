dfc750e941dd95811839c02b8493c8c9d464c67c
Latrevion <xiaopikaqiu@outlook.com>
2022-11-03 21:50:03 +0800

    添加缝隙

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,48 +23,50 @@ export default {
 <style lang="scss" scoped>
 @import "../views/_helper.scss";
 
- .home {
-     flex: 1;
-     height: 100%;
-     display: flex;
-     flex-direction: column;
+.home {
+    flex: 1;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
 
-     >.header {
-         background-size: cover;
-         height: px(99);
-     }
+    >.header {
+        background-size: cover;
+        height: px(99);
+    }
 
-     >.main {
-         flex: 1;
-         display: grid;
-         grid-template:
-             "box1 box2 box4 box5" 755fr
-             "box3 box3 box4 box5" 363fr / 366fr 361fr 811fr 747fr;
+    >.main {
+        flex: 1;
+        display: grid;
+        grid-template:
+            "box1 box2 box4 box5" 755fr
+            "box3 box3 box4 box5" 363fr / 366fr 361fr 811fr 747fr;
+        grid-column-gap: px(28);
+        grid-row-gap: px(28);
 
-         >.section1 {
-             grid-area: box1;
-             background: pink;
-         }
+        >.section1 {
+            grid-area: box1;
+            background: pink;
+        }
 
-         >.section2 {
-             grid-area: box2;
-             background: lightgray;
-         }
+        >.section2 {
+            grid-area: box2;
+            background: lightgray;
+        }
 
-         >.section3 {
-             grid-area: box3;
-             background: lightblue;
-         }
+        >.section3 {
+            grid-area: box3;
+            background: lightblue;
+        }
 
-         >.section4 {
-             grid-area: box4;
-             background: lightcyan;
-         }
+        >.section4 {
+            grid-area: box4;
+            background: lightcyan;
+        }
 
-         >.section5 {
-             grid-area: box5;
-             background: lightyellow;
-         }
-     }
- }
+        >.section5 {
+            grid-area: box5;
+            background: lightyellow;
+        }
+    }
+}
 </style>
