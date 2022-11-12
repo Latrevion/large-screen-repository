58f77ca4947abe524c6d03487dcc1fa46ca40a7b
Latrevion <xiaopikaqiu@outlook.com>
2022-11-12 22:44:13 +0800

    动态更新chart10

--- src/components/chart-10.vue
+++ src/components/chart-10.vue
@@ -8,13 +8,30 @@ const px = (n) => { return n / 2420 * pageWidth }
 export default {
   data() {
     return {
+      newData: [],
+      data: [
+        { value: 50 },
+        { value: 22 },
+        { value: 20 },
+        { value: 18 },
+        { value: 27 }]
     }
   },
   mounted() {
-    this.initChart()
+    this.initChart(this.data)
+    this.timer= setInterval(() => {
+      this.newData = [
+        { value: Math.random() * 6 + 40 },
+        { value: Math.random() * 15 + 22 },
+        { value: Math.random() * 11 + 20 },
+        { value: Math.random() * 22 + 18 },
+        { value: Math.random() * 8 + 27 }]
+      this.data = this.newData
+      this.initChart(this.data)
+    }, 3000)
   },
   methods: {
-    initChart() {
+    initChart(data) {
       var myChart = echarts.init(document.querySelector('.chart10'));
       // 绘制图表
       myChart.setOption({
@@ -63,14 +80,14 @@ export default {
         legend: { show: false },
         series: [{
           type: 'bar',
-          data: [40, 22, 20, 18, 32],
-            color: new echarts.graphic.LinearGradient(
-              0, 0, 1, 0,
-              [
-                { offset: 1, color: '#2034f9' },
-                { offset: 0, color: '#04a1ff' }
-              ]
-            )
+          data: data.map(i=>i.value),
+          color: new echarts.graphic.LinearGradient(
+            0, 0, 1, 0,
+            [
+              { offset: 1, color: '#2034f9' },
+              { offset: 0, color: '#04a1ff' }
+            ]
+          )
         }]
       })
     }
